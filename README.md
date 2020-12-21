5495852ccc9d61f10763454a23220d729cf58dfc
Grant Carthew <grant@carthew.net>
2020-12-21 14:40:52 +1000

    Initial content commit

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
-# deno-htmlsync
-A static website utility to synchronize the HTML header and footer to all your HTML files.
+# htmlsync
+
+Synchronize the HTML header and footer to all your HTML files.
+
+A true static site should not require building at all. There is one problem, how do you
+configure the same header and footer on every HTML file? You could copy and paste, however that
+is a manual process which can introduce errors.
+
+Using `htmlsync` you can specify a source HTML file, which holds the header and footer you want synchronized,
+and it will replace the header and footer in every HTML file.
+
+## How does it work?
+
+The `htmlsync` script looks for two specific token strings in the HTML files. The first string is
+required to define the header of the HTML document. The second string is optional and defines the footer of the HTML document.
+
+Here are what the token strings look like in a valid `htmlsync` file:
+
+```html
+<!--
+  Any text above the head token is defined as the htmlsync header content.
+  The head token is required for htmlsync to process this file.
+  -->
+
+
+@SyncTokenHead
+
+<!--
+  Any text between the head token and the foot token is the page content.
+  This content will be ignored by htmlsync.
+  -->
+
+@SyncTokenFoot
+
+<!--
+  Any text after the foot token is defined as the htmlsync footer content.
+  The foot token is optional.
+  -->
+```
+
+All content from the start of the `.html` file up to and including the line with the head token
+is defined as the header.
+
+All content from the line with the foot token to the end of the `.html` file is defined as the
+footer.
+
+The `htmlsync` script only supports files that meet the following criteria:
+
+* The file is in the same directory that `htmlsync` was executed in.
+* The file has an extension of `.html`.
+* The file has the `@SyncTokenHead` string in its content.
+
+There are two uses for `htmlsync`:
+
+1. Synchronize the header and footer from the source file to all supported files.
+1. Create a new file with the header and footer from the source file as its content.
+
+## Getting Started
+
+Make sure you have the latest version of [Deno](https://deno.land/) installed.
+
+Edit all your HTML files and ensure you have at least a head token and optionally a foot token.
+
+
+To execute `htmlsync` use the following command: 
+
+```batch
+deno run --unstable --allow-read=. --allow-write=. https://github.com/grantcarthew/deno-htmlsync/htmlsync.js [-h | --help] <source file> [new file]
+```
+
+The only currently supported option is `-h` or `--help` which displays the command line help.
+
+_Suggestion: It's a long command. Place it into a `bash` file to make repeated use easier._
+
+### How to sync the header and footer
+
+To synchronize the header and footer from a `source file` to all `.html` files in your current directory simply specify a source file name only. For example:
+
+```batch
+deno run --unstable --allow-read=. --allow-write=. https://github.com/grantcarthew/deno-htmlsync/htmlsync.js index.html
+```
+
+The above command will read the header and footer from the `index.html` file and apply that header and footer to
+all `.html` files found in the same directory.
+
+It is worth noting here that any `.html` files that do not have a head token will be ignored.
+
+### Creating a new HTML file
+
+Here is an example of using `htmlsync` to create a new `.html` file:
+
+```batch
+deno run --unstable --allow-read=. --allow-write=. https://github.com/grantcarthew/deno-htmlsync/htmlsync.js index.html about.html
+```
+
+The above command will read the header and footer content from the `index.html` file and create a new file named `about.html` that contains the header and footer content. The new file will have both the head and foot tokens.
+
+## Contributing
+
+If you find a bug please open an issue or send a pull request.
+
+Review the [Code of Conduct](CONTRIBUTING.md) document.
+
+## Change Log
+
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ htmlsync.js
@@ -0,0 +1,152 @@
+import { existsSync } from "https://deno.land/std@0.81.0/fs/mod.ts";
+
+console.time("Run Time");
+const tokens = {
+  head: "@SyncTokenHead",
+  foot: "@SyncTokenFoot",
+};
+
+// Argument Checks
+if (Deno.args.length < 1) {
+  exitWithError("Source file argument missing");
+}
+
+if (Deno.args[0] === '-h' || Deno.args[0] === '--help') {
+  printHelp();
+  Deno.exit(0);
+}
+
+if (!Deno.args[0].endsWith(".html")) {
+  exitWithError(`Source file extension is not .html: ${Deno.args[0]}`);
+}
+
+if (!existsSync(Deno.args[0])) {
+  exitWithError(`Source file not found: ${Deno.args[0]}`);
+}
+
+let newFile = false;
+if (Deno.args.length === 2) {
+  newFile = true;
+  console.log(`Creating new HTML file: ${Deno.args[1]}`);
+}
+
+if (newFile && existsSync(Deno.args[1])) {
+  exitWithError(`New file already exists: ${Deno.args[1]}`);
+}
+
+const sourceHtml = await Deno.readTextFile(Deno.args[0]);
+const sourceLineIndexes = getTokenLineIndexes(sourceHtml, tokens);
+
+if (sourceLineIndexes.headTokenNextLineIndex < 0) {
+  exitWithError(`Head token ${tokens.head} missing`);
+}
+
+// Sync HTML Content
+console.log("htmlsync v1.0.0");
+
+let header = sourceHtml.slice(0, sourceLineIndexes.headTokenNextLineIndex);
+let footer = "";
+if (sourceLineIndexes.footTokenLineIndex > 0) {
+  footer = sourceHtml.slice(
+    sourceLineIndexes.footTokenLineIndex,
+    sourceHtml.length,
+  );
+}
+
+if (newFile) {
+  Deno.writeTextFileSync(Deno.args[1], header + footer);
+  console.log("New HTML file created");
+  console.timeEnd("Run Time");
+  Deno.exit();
+}
+
+const files = Deno.readDirSync(".");
+let htmlFiles = [];
+for (const item of files) {
+  if (
+    item.isFile &&
+    item.name.endsWith(".html") &&
+    item.name !== Deno.args[0]
+  ) {
+    htmlFiles.push(item.name);
+  }
+}
+
+for (const htmlFile of htmlFiles) {
+  const targetHtml = await Deno.readTextFile(htmlFile);
+  const targetLineIndexes = getTokenLineIndexes(targetHtml, tokens);
+  console.dir(targetLineIndexes);
+  let updatedHtml = header;
+  if (targetLineIndexes.headTokenNextLineIndex > 0) {
+    if (targetLineIndexes.footTokenLineIndex > 0) {
+      updatedHtml += targetHtml.slice(
+        targetLineIndexes.headTokenNextLineIndex,
+        targetLineIndexes.footTokenLineIndex,
+      );
+      updatedHtml += footer;
+    } else {
+      console.log('header only')
+      updatedHtml += targetHtml.slice(
+        targetLineIndexes.headTokenNextLineIndex,
+        targetHtml.length,
+      );
+    }
+    Deno.writeTextFileSync(htmlFile, updatedHtml);
+    console.log(`Synchronized html file: ${htmlFile}`);
+  } else {
+    console.log(`HTML file sync token missing: ${htmlFile}`);
+  }
+}
+
+function printHelp () {
+  console.log(`
+  
+  htmlsync - Synchronize the HTML header and footer to all your HTML files
+
+  CLI:
+    deno run --unstable --allow-read=. --allow-write=. https://github.com/grantcarthew/deno-htmlsync/htmlsync.js [-h | --help] <source file> [new file] 
+  
+  Usage: htmlsync.js [options] source [new-file]
+    [Options]       See the options below
+    <source file>   The ".html" file with the header and footer you would like to synchronize
+    [new file]      Optional. If present a single new file will be created from the source
+
+  Options:
+    -h, --help    Display this help
+
+  `)
+}
+
+function exitWithError(message) {
+  console.error(message);
+  Deno.exit(1);
+}
+
+function getTokenLineIndexes(html, tokens) {
+  const headTokenIndex = html.indexOf(tokens.head);
+  const footTokenIndex = html.lastIndexOf(tokens.foot);
+  const result = {
+    headTokenNextLineIndex: -1,
+    footTokenLineIndex: -1,
+  };
+
+  if (headTokenIndex > 0) {
+    for (let i = headTokenIndex; i < html.length; i++) {
+      if (html[i] === "\n") {
+        result.headTokenNextLineIndex = i + 1;
+        break;
+      }
+    }
+  }
+  if (footTokenIndex > 0) {
+    for (let i = footTokenIndex; i > 0; i--) {
+      if (html[i] === "\n") {
+        result.footTokenLineIndex = i;
+        break;
+      }
+    }
+  }
+  return result;
+}
+
+console.timeEnd("Run Time");
